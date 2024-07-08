713f824e9ed41ec267a191a9ef4fabfe4371dcef
Matthew Lastovka <matthewlastovka@Matthews-MacBook-Pro-9.local>
2024-07-08 10:24:16 -0400

    write setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+from tessffisearch import __version__
+
+setup(
+	name='tessffisearch',
+	version=__version__,
+	url="https://github.com/mattlastovka/tessffisearch",
+	author="Matthew Lastovka",
+	author_email="mlastovk@umd.edu",
+	py_modules=['tessffisearch'],
+	)
