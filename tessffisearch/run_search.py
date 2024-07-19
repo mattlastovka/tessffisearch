e50be5066dd4d4af9cfccbf25c92e894d6c7c52a
mattlastovka <105942230+mattlastovka@users.noreply.github.com>
2024-07-19 15:36:43 -0400

    Update run_search.py

--- tessffisearch/run_search.py
+++ tessffisearch/run_search.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
         print("TESSmag", target_list['Tmag'][tic_index])
         mass = target_list['mass'][tic_index]
         radius = target_list['rad']
-        run_the_search(ticid, mass, radius)
+        run_the_search(ticid, mass=None, radius=None)
         finish_file = './transit_search/finished_runs.txt'
         finfile = open(finish_file, "a")  # append mode
         finfile.write(str(tic_index) + " " + str(ticid) + '\n')
