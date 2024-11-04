7e61f594b106a280baa2148e16205c1bb3dd2ef8
mattlastovka <105942230+mattlastovka@users.noreply.github.com>
2024-11-04 10:14:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='tessffisearch',
-	version="0.0.1",
+	version="0.1.0",
 	url="https://github.com/mattlastovka/tessffisearch",
 	author="Matthew Lastovka",
 	author_email="mlastovk@umd.edu",
